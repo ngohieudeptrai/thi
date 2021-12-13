ba9e66942b8823c173a5796a406f228c290ee0b2
ngohieudeptrai <94547279+ngohieudeptrai@users.noreply.github.com>
2021-12-13 12:08:12 +0700

    Add files via upload

---
+++ baithi.js
@@ -0,0 +1,34 @@
+
+var menu =[];
+
+var douong={
+    'id':'ma san pham',
+    'name':'do uong',
+    'size':'size',
+    'price':'gia tien'
+};
+menu.push(douong)
+    douong={
+    'id':'01',
+    'name':'ca phe den',
+    'size':'xl',
+    'price':'3000'
+};
+menu.push(douong);
+douong={
+    'id':'02',
+    'name':'ca phe sua',
+    'size':'xl',
+    'price':'4000'
+}
+menu.push(douong);
+douong={
+    'id':'03',
+    'name':'tra da',
+    'size':'s',
+    'price':'2000'
+}
+menu.push(douong);
+if( document.getElementById("loaidouong") == 1){
+
+}
